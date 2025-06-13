efa2e4972e669a0075de8d750d2afeb2b41e18b6
Andrea Fassina <fasenderos@gmail.com>
2025-06-13 10:50:01 +0200

    wip: remove old params

--- adapters/nativery/nativery.go
+++ adapters/nativery/nativery.go
@@ -18,59 +18,17 @@ type adapter struct {
 	endpoint string
 }
 
-type refRef struct {
-	Page string `json:"page"`
-	Ref  string `json:"ref"`
-}
-
-// request body to send to widget server in ext
-type nativeryExtReqBody struct {
-	Id     string `json:"id"` //the placement/widget id
-	Xhr    int    `json:"xhr"`
-	V      int    `json:"v"`
-	Ref    string `json:"ref"`
-	RefRef refRef `json:"refref"`
-}
-
-type impExt struct {
-	Nativery nativeryExtReqBody `json:"nativery"`
-}
-
 type bidReqExtNativery struct {
 	IsAMP    bool   `json:"isAmp"`
 	WidgetId string `json:"widgetId"`
-}
-
-type ImpressionBody struct {
-	WidgetId     string   `json:"idw"`
-	UrlId        string   `json:"idu"`
-	ImpressionId string   `json:"idi"`
-	AdIds        []string `json:"ida"`
-	IsAMP        bool     `json:"isAmp"`
-	Ref          string   `json:"ref"`
-	RefRef       string   `json:"refref"`
-	SessionId    string   `json:"sid"`
-}
-
-type ImpressionOptions struct {
-	Method string         `json:"method"`
-	Body   ImpressionBody `json:"body"`
-}
-
-type ImpressionData struct {
-	Url     string            `json:"url"`
-	Options ImpressionOptions `json:"options"`
 }
 
 type bidExtNativery struct {
 	BidType       string   `json:"bid_ad_media_type"`
 	BidAdvDomains []string `json:"bid_adv_domains"`
 
-	AdvertiserId     string         `json:"adv_id,omitempty"`
-	BrandCategory    int            `json:"brand_category_id,omitempty"`
-	Impression       ImpressionData `json:"impression"`
-	RealImpression   ImpressionData `json:"realImpression"`
-	RealImpressionAd ImpressionData `json:"realImpressionAd"`
+	AdvertiserId  string `json:"adv_id,omitempty"`
+	BrandCategory int    `json:"brand_category_id,omitempty"`
 }
 
 type bidExt struct {
@@ -117,21 +75,7 @@ func (a *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.E
 			widgetId = nativeryExt.WidgetId
 		}
 
-		impCopy := imp
-		impCopy.Ext, err = jsonutil.Marshal(impExt{Nativery: nativeryExtReqBody{
-			Id:     nativeryExt.WidgetId,
-			Xhr:    2,
-			V:      3,
-			Ref:    reqCopy.Site.Page,
-			RefRef: refRef{Page: reqCopy.Site.Page, Ref: reqCopy.Site.Ref},
-		}})
-
-		if err != nil {
-			errs = append(errs, err)
-			continue
-		}
-
-		validImps = append(validImps, impCopy)
+		validImps = append(validImps, imp)
 	}
 
 	reqCopy.Imp = validImps
